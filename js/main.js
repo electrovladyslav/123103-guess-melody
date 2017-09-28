b98cc7cb47e7b845083555bec69fdec233d41174
Yermolin Vlad <electrovladyslav@gmail.com>
2017-09-28 10:01:30 +0300

    Сделал функции для переключения экранов по нажатию на стрелки.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,49 @@
-function getScreens() {
-  const template = document.querySelector(`template`);
-  const arr = [];
-  template.content.querySelectorAll(`.main`).forEach( (item) => {
-    arr.push(item);
+(function () {
+  function getScreens() {
+    const template = document.querySelector(`template`);
+    const arr = [];
+    template.content.querySelectorAll(`.main`).forEach((item) => {
+      arr.push(item);
+    });
+    return arr;
+  }
+
+  const screens = getScreens();
+
+  function showScreen(screenNumber) {
+    const main = document.querySelector(`.main`);
+    if (screens[screenNumber] !== void (0)) {
+      main.innerHTML = ``;
+      main.appendChild(screens[screenNumber].cloneNode(true));
+    }
+  }
+
+  let currentScreen = 0;
+  showScreen(currentScreen);
+
+  document.addEventListener(`keydown`, (event) => {
+    if (event.altKey === true) {
+      if (event.key === `ArrowRight`) {
+        switchScreen(`next`);
+      }
+      if (event.key === `ArrowLeft`) {
+        switchScreen(`prev`);
+      }
+    }
   });
-  return arr;
-}
 
-const screens = getScreens();
-
-function showScreen(screenNumber) {
-  const main = document.querySelector(`.main`);
-  main.innerHTML = ``;
-  if (screens[screenNumber] !== undefined) {
-    main.appendChild(screens[screenNumber].cloneNode(true));
-   }
-}
-
-showScreen(0);
+  function switchScreen(direction) {
+    switch (direction) {
+      case `next` :
+        if ((currentScreen + 1) < screens.length) {
+          showScreen(++currentScreen);
+        }
+        break;
+      case `prev` :
+        if ((currentScreen - 1) >= 0) {
+          showScreen(--currentScreen);
+        }
+        break;
+    }
+  }
+})();
