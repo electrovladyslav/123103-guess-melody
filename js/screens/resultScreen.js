18cc6f5c2703457727a847586a03a0b84c4d7ae3
Yermolin Vlad <electrovladyslav@gmail.com>
2017-10-28 18:34:32 +0300

    Добавил загрузку данных на сервер и скачивания результатов с сервера.

--- js/screens/resultScreen.js
+++ js/screens/resultScreen.js
@@ -1,10 +1,14 @@
 import renderScreen from '../functions/renderScreen';
+import calcPoints from '../functions/calcPoints';
+
 import ResultView from '../view/resultView';
 import app from '../app';
+import Loader from '../loader';
 
 export default class {
   constructor(state) {
     this._view = new ResultView(state);
+    this._state = state;
   }
 
   init() {
@@ -12,6 +16,22 @@ export default class {
       app.startGame();
     };
 
-    renderScreen(this._view.element);
+    if ((this._state.lives > 0) || (this._state.time > 0)) {
+      this._view.points = calcPoints(this._state.answers, this._state.lives);
+      Loader.loadResults()
+          .then((results) => {
+            this._view.otherResults = results;
+          })
+          .then(() => {
+            Loader.saveResults({
+              time: this._state.time,
+              points: this._view.points
+            }).then(() => {
+              renderScreen(this._view.element);
+            });
+          });
+    } else {
+      renderScreen(this._view.element);
+    }
   }
 }
